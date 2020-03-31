a45840a5029ea544a1beee7662e7344bd15a173c
Andrew J. Leer <leeand00@hotmail.com>
2020-03-30 20:27:20 -0400

    Added export scripts and current output from them.

---
+++ export_png.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+for x in (ls ./src/dia/*.dia)
+do 
+	dia -e ./export/png/$x.png $x;   
+	echo $x; 
+done
---
+++ export_svg.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+for x in (ls dia/*.dia) 
+do 
+	dia -e ./export/svg/$x.svg $x; 
+	echo $x; 
+done
